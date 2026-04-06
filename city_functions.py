f60dcff3b247ebe56d0ae7e1e47632c1ee3e2f98
Rajani <rajani.kami004@gmail.com>
2026-04-05 21:57:29 -0400

    def city country

---
+++ city_functions.py
@@ -0,0 +1,10 @@
+""""Author: Rajani Baraili
+Date: 04-05-2026
+Purpose:function returns formatted city and country name as readable string """
+
+def city_country(city, country):
+    result = f"{city.name}, {country.name}"
+  
+
+
+    
