e631797baf77c529142f9fb873bc10e54677fa99
Rajani <rajani.kami004@gmail.com>
2026-04-05 22:11:54 -0400

    def input city country

--- city_functions.py
+++ city_functions.py
@@ -3,14 +3,24 @@ Date: 04-05-2026
 Purpose:function returns formatted city and country name as readable string """
 
 """Return a formatted string in the format of 'City, Country – population xxx'."""
-def city_country(city, country):
-    result = f"{city.name}, {country.name}"
 
-    if city.population:
-        result += f" – population {city.population}"
-        return result
+
+def input_city_country():
+        city_name = input("Enter the name of the city: ")
+        country_name = input("Enter the name of the country: ")
+        population = input("Enter the population (optional): ")
+
+        def city_country(city, country, population=None):
+            result = f"{city.name}, {country.name}"
+            if population is not None:
+                result += f", {population}"
+            return result
+
+input_city_country()
     
-    
+
+
+
   
 
 
