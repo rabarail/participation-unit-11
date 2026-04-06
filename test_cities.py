960832a6b6a6e772d494def6c18319c6de35923c
Rajani <rajani.kami004@gmail.com>
2026-04-05 22:28:39 -0400

    def test city country population

--- test_cities.py
+++ test_cities.py
@@ -3,6 +3,10 @@
 def test_city_country():
     result = test_city_country("Santiago", "Chile")
     assert result == "Santiago, Chile"
-    
+"""Test with just city and country."""
+def test_city_country_population():
+    result = test_city_country("Santiago", "Chile", population=5000000)
+    assert result == "Santiago, Chile, - population 5000000"  
+"""Test with city, country, and population."""
 
 
