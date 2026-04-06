9683299d6d9a788aaefcc0254bdbd620063a0acd
Rajani <rajani.kami004@gmail.com>
2026-04-05 22:19:41 -0400

    fix error

--- city_functions.py
+++ city_functions.py
@@ -5,19 +5,24 @@ Purpose:function returns formatted city and country name as readable string """
 """Return a formatted string in the format of 'City, Country – population xxx'."""
 
 
+from unittest import result
+
+
 def input_city_country():
         city_name = input("Enter the name of the city: ")
         country_name = input("Enter the name of the country: ")
         population = input("Enter the population (optional): ")
 
         def city_country(city, country, population=None):
-            result = f"{city.name}, {country.name}"
+            result = f"{city}, {country}"
             if population is not None:
                 result += f", {population}"
             return result
+        result = city_country(city_name, country_name, population)
+        print(result)
 
 input_city_country()
-    
+
 
 
 
